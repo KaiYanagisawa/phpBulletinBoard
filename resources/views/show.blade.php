e1dfeaa5a71cab0264e9664cb1f57d03bc79f51b
KaiYanagisawa <12730ky@gmail.com>
2020-12-15 03:08:56 +0900

    edit views routes

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -9,6 +9,7 @@
     <p>{{ $article->user_name }}</p>
     <p>
         <a href={{ route('article.list') }} class="btn btn-outline-primary">一覧に戻る</a>
+        <a href={{ route('article.edit', ['id' => $article->id]) }} class="btn btn-outline-primary">編集</a>
     </p>
     <div>
         {{ Form::open(['method' => 'delete', 'route' => ['article.delete', $article->id]]) }}
